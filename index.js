0b61abb9892bc050e4a81305b947120ed20e516f
Droplessrain <126644063+Droplessrain@users.noreply.github.com>
2024-07-26 11:44:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -40,22 +40,6 @@ function previousSlide() {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-/* Функция перелистывания */
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("item");   
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-  /* Проходим по каждому слайду в цикле for */
-    for (let slide of slides) {
-        slide.style.display = "none";
-    }   
-    slides[slideIndex - 1].style.display = "block"; 
-}
 
 /*Пробный вариант слайдера на полном экране*/
 /*imgInTable1*/
